fc40852330996f798174899625a2d6bed518ac13
Edredon <lucianoteoluizagasiete@gmail.com>
2022-07-19 18:56:01 -0300

    agregue el esquema y el modelo de productos

---
+++ server/models/Product.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  category:  String,
+  name: String,
+  desc: String,
+  price: Number,
+  discount_percentage:  Number,
+  discount_price_fixed: Number,
+  stock: Number,
+  img: String,
+});
+
+const Product = mongoose.model('Product', schema);
+
+export default Product;
