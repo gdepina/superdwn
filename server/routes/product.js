44cbffee9b045469be95d5f04971022eeeee0d88
Remigio Furini <remi.furini@hotmail.com>
2022-07-28 06:19:15 +0200

    middleware para la validacion de usuario loggeado

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,12 +1,15 @@
 
 import express from "express";
 import Product from '../controllers/product'
+import isLogged from '../middleware/auth'
 
 const router = express.Router();
 
 const prefix = '/api/products'
 
-router.get(prefix, Product.list);
+//ruta de prueba con el middleware isLogged implementado
+router.get(prefix, isLogged, Product.list);
+
 router.post(prefix, Product.create);
 router.put(prefix, Product.update);
 router.delete(prefix, Product.destroyAss);
