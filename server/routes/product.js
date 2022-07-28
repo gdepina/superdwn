c07518da85e1332a8aabaf0fdd8e365ec769b76d
Remigio Furini <remi.furini@hotmail.com>
2022-07-28 07:05:24 +0200

    aplicando los consejos de ger en el islogged middleware

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,14 +1,14 @@
 
 import express from "express";
 import Product from '../controllers/product'
-import isLogged from '../middleware/auth'
+import auth from '../controllers/auth'
 
 const router = express.Router();
 
 const prefix = '/api/products'
 
 //ruta de prueba con el middleware isLogged implementado
-router.get(prefix, isLogged, Product.list);
+router.get(prefix, auth.isLogged, Product.list);
 
 router.post(prefix, Product.create);
 router.put(prefix, Product.update);
