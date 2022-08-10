a1e3810026b8c2015a0f98de2a928f0b56cf51a5
Remigio Furini <remi.furini@hotmail.com>
2022-08-10 17:24:35 +0200

    ultimos cambios al linter

--- client/components/context/LoginProvider.js
+++ client/components/context/LoginProvider.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 
 const LoginContext = React.createContext();
 
-const LoginProvider = (props) => {
-  const { children } = props;
+const LoginProvider = ({ children }) => {
   const loginStates = {
     LOGGED: 0,
     LOGGED_OUT: 1,
   };
+
   const [isLogged, setIsLogged] = React.useState(loginStates.LOGGED_OUT);
+
   const loginProviderValue = React.useMemo(() => (
-    { isLogged, setIsLogged }), [isLogged, setIsLogged]);
+    { isLogged, setIsLogged }), [isLogged]);
+
   return (
     <LoginContext.Provider value={loginProviderValue}>
       {children}
