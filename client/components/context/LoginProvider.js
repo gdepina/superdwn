40a344e253a25dd0d80012b6d8eaab16c38a501a
Remigio Furini <remi.furini@hotmail.com>
2022-08-10 18:01:32 +0200

    ultimo commit perdon estoy re fumado

--- client/components/context/LoginProvider.js
+++ client/components/context/LoginProvider.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 
 const LoginContext = React.createContext();
 
-const LoginProvider = ({ children }) => {
-  const loginStates = {
-    LOGGED: 0,
-    LOGGED_OUT: 1,
-  };
+const loginStates = {
+  LOGGED: 0,
+  LOGGED_OUT: 1,
+};
 
+const LoginProvider = ({ children }) => {
   const [isLogged, setIsLogged] = React.useState(loginStates.LOGGED_OUT);
 
   const loginProviderValue = React.useMemo(() => (
-    { isLogged, setIsLogged }), [isLogged]);
+    { isLogged, setIsLogged, loginStates }), [isLogged]);
 
   return (
     <LoginContext.Provider value={loginProviderValue}>
