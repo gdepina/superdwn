1541efa9dfcbec608ba5940a81f61e3f353bb4b6
Ger <german.depina@mercadolibre.com>
2022-08-11 01:19:18 -0300

    fix lint 1

--- client/components/Menu/Menu.js
+++ client/components/Menu/Menu.js
@@ -1,87 +1,23 @@
-import React, { useState } from 'react';
-import {
-  Navbar,
-  Container,
-  ScrollArea,
-  NavLink,
-  Button,
-  Group,
-  Avatar,
-  Text,
-  UnstyledButton,
-  Modal,
-  TextInput,
-} from '@mantine/core';
+import React, { useState, useContext } from 'react';
+import { Navbar, NavLink, ScrollArea } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconHome, IconShoppingCart } from '@tabler/icons';
 import { Link, useLocation } from 'react-router-dom';
 import menuJss from './menu-jss';
-import { signIn } from './login-api';
+import Profile from './Profile';
+import SignUp from './SignUp';
+import { AuthContext } from '../Providers/AuthProvider';
 
 const menuOptions = [
   { label: 'Productos', icon: <IconHome />, to: '/' },
   { label: 'Carrito', icon: <IconShoppingCart />, to: '/cart' },
 ];
 
-const logIn = (opts) => {
-  const {
-    setProfile, profile, executeLogIn, form,
-  } = opts;
-  return (
-    <Container p="xl" align="center" pb="xs">
-      <Modal centered opened={profile.opened} onClose={() => setProfile({ opened: false, user: {} })} title="Inicia Sesion">
-        <form onSubmit={form.onSubmit((values) => executeLogIn(values))}>
-          <TextInput required label="Email" placeholder="your@email.com" {...form.getInputProps('email')} />
-          <TextInput required label="Contraseña" placeholder="Contraseña" {...form.getInputProps('password')} />
-
-          <Group position="right" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
-        </form>
-      </Modal>
-      <Button size="xs" mr="xl" onClick={() => setProfile({ opened: true, user: {} })}>
-        Iniciar Sesion
-      </Button>
-      <Button size="xs">Registrarse</Button>
-    </Container>
-  );
-};
-
-const loggedIn = (opts) => {
-  const { setProfile, profile } = opts;
-  const {
-    email, userName, name, surname,
-  } = profile.user;
-  return (
-    <Container p="xl" align="center" pb="xs">
-      <UnstyledButton py="xl">
-        <Group>
-          <Avatar size={40} color="blue">
-            {name.charAt(0).toUpperCase()}
-            {surname.charAt(0).toUpperCase()}
-          </Avatar>
-          <div>
-            <Text>{userName}</Text>
-            <Text size="xs" mr="xl" color="dimmed">
-              {email}
-            </Text>
-          </div>
-        </Group>
-      </UnstyledButton>
-      <Button size="xs" mr="xl" onClick={() => setProfile({ user: {} })}>
-        Salir
-      </Button>
-    </Container>
-  );
-};
-
 const Menu = () => {
   const { classes } = menuJss();
   const location = useLocation();
-  const [profile, setProfile] = useState({
-    user: {},
-    opened: false,
-  });
+  const [opened, setOpened] = useState(false);
+  const { logIn, user, logOut } = useContext(AuthContext);
 
   const form = useForm({
     initialValues: {
@@ -93,12 +29,6 @@ const Menu = () => {
     },
   });
 
-  const executeLogIn = (data) => {
-    signIn(data).then((result) => {
-      setProfile({ user: result.user, opened: false });
-    }).catch((err) => console.log(err));
-  };
-
   const navLinks = menuOptions.map((item) => (
     <NavLink
       {...item}
@@ -109,14 +39,20 @@ const Menu = () => {
     />
   ));
 
+  const onSubmitHandler = (data) => {
+    logIn(data, () => setOpened(false));
+  };
+
   const opts = {
     classes,
-    setProfile,
+    opened,
     navLinks,
     location,
-    profile,
-    executeLogIn,
+    setOpened,
+    onSubmit: onSubmitHandler,
+    onExit: logOut,
     form,
+    user,
   };
 
   const {
@@ -129,7 +65,7 @@ const Menu = () => {
         <div className={linksInner}>{navLinks}</div>
       </Navbar.Section>
       <Navbar.Section className={footer}>
-        {Object.keys(profile.user).length === 0 ? logIn(opts) : loggedIn(opts)}
+        {Object.keys(user).length === 0 ? SignUp(opts) : Profile(opts)}
       </Navbar.Section>
     </Navbar>
   );
