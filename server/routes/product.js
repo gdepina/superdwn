43bf21bc989b98cd9b5a9dd9a8f925e3c060ae3c
Remigio Furini <remi.furini@hotmail.com>
2022-08-17 22:29:31 +0200

    api simple para mostrar detalle de un producto para que santi pueda avanzar

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 const prefix = '/api/products';
 router.get(prefix, Product.list);
+router.get(`${prefix}/:id`, Product.detail);
 router.post(prefix, Product.create);
 router.put(prefix, Product.update);
 router.delete(prefix, Product.destroyAss);
