c453206a86b0118d690c73e3fb6f5c2603aff9b1
Remigio Furini <remi.furini@hotmail.com>
2022-08-27 03:55:25 +0200

    manejo de errores en el fetch

--- client/apis/product-api.js
+++ client/apis/product-api.js
@@ -3,9 +3,13 @@ const fetchProducts = () =>
     method: 'GET',
   })
     .then((response) => response.json())
+    // eslint-disable-next-line no-console
     .catch((err) => console.log(err));
 
 const fetchProductDetail = async (id) => {
+  // ya se que falta manejar los errores pero estaria bueno q lo haga otro
+  // habria que mejorar la api para que devuelva CODE en las diferentes respuestas
+  // idea: podriamos armar un hook useFetch?
   const response = await fetch(`/api/products/${id}`, { method: 'GET' });
   const productDetail = await response.json();
   return productDetail;
