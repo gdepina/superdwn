b4f1dba4666d65925f36ccbb324e66fb3c60fa7f
remifu93 <84943791+remifu93@users.noreply.github.com>
2022-08-30 23:18:26 +0200

    implementado el async await y agregado un fecth de ejemplo (#42)

--- client/apis/product-api.js
+++ client/apis/product-api.js
@@ -3,26 +3,20 @@ const fetchProducts = () =>
     method: 'GET',
   })
     .then((response) => response.json())
-    .catch((err) => console.log(err))
+    .catch((err) => err)
     .then((data) => {
       const newData = data.map((product) => {
-        // eslint-disable-next-line max-len, no-param-reassign
-        product.priceFixed = Math.trunc(product.price - (product.price * product.discount_percentage) / 100);
-        return product;
+        const { price, discount_percentage: discount } = product;
+        const priceFixed = Math.trunc(price - (price * discount) / 100);
+        return { ...product, priceFixed };
       });
       return newData;
     });
 
-const fetchProductDetail = (id) =>
-  fetch(`/api/products/${id}`, {
-    method: 'GET',
-  })
-    .then((response) => response.json())
-    .catch((err) => console.log(err))
-    .then((data) => {
-      // eslint-disable-next-line no-param-reassign
-      data.priceFixed = Math.trunc(data.price - (data.price * data.discount_percentage) / 100);
-      return data;
-    });
-// eslint-disable-next-line import/prefer-default-export
+const fetchProductDetail = async (id) => {
+  const response = await fetch(`/api/products/${id}`, { method: 'GET' });
+  const productDetail = await response.json();
+  return productDetail;
+};
+
 export { fetchProducts, fetchProductDetail };
